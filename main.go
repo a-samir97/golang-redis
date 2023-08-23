df8c5d80cc0c5e5dd4b629113df93982f5c1580c
a.samir97 <a.samir9710@gmail.com>
2023-08-23 18:11:30 +0300

    Implement PING command (#3)
    Co-authored-by: Ahmed Samir <a.samir9710@gmail.com>

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
@@ -34,9 +35,29 @@ func main() {
 			fmt.Println(err)
 			return
 		}
-		_ = value
+		// check about type of the client input
+		if value.typ != "array" {
+			fmt.Println("Invalid request, expected array type")
+			continue
+		}
+		// check array length
+		if len(value.array) == 0 {
+			fmt.Println("Invalid request, expected array length greater than 0")
+			continue
+		}
+		command := strings.ToUpper(value.array[0].bulk)
+		args := value.array[1:]
 
 		writer := NewWriter(conn)
-		writer.Write(Value{typ: "string", str: "OK"})
+
+		handler, ok := Handlers[command]
+
+		if !ok {
+			fmt.Println("Invalid command: ", command)
+			writer.Write(Value{typ: "string", str: ""})
+			continue
+		}
+		result := handler(args)
+		writer.Write(result)
 	}
 }
