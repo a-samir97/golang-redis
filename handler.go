6e672e9d866ba548c235f39905251911e2cb88e6
a.samir97 <a.samir9710@gmail.com>
2023-08-23 20:54:57 +0300

    Added SET and GET redis commands (#5)

--- handler.go
+++ handler.go
@@ -2,7 +2,11 @@ package main
 
 var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
+	"SET":  set,
+	"GET":  get,
 }
+
+var SETs = map[string]string{}
 
 // PING Command
 func ping(args []Value) Value {
@@ -10,4 +14,32 @@ func ping(args []Value) Value {
 		return Value{typ: "string", str: "PONG"}
 	}
 	return Value{typ: "string", str: args[0].bulk}
+}
+
+func set(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number for SET args"}
+	}
+	key := args[0].bulk
+	value := args[1].bulk
+
+	SETs[key] = value
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func get(args []Value) Value {
+	if len(args) != 1 {
+		return Value{typ: "error", str: "ERR wrong number for SET args"}
+	}
+
+	key := args[0].bulk
+
+	value, ok := SETs[key]
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
 }
